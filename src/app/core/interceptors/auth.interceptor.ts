06c19cd2a98a02ffa9b9db2bc680946f388f2939
Tomas Campo <tcampof@gmail.com>
2024-06-16 19:11:30 +0200

    Configuración interceptor

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -4,9 +4,13 @@ import { AuthService } from '../services/auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
     const auth = inject(AuthService);
+    const username = '11181750';
+    const password = '60-dayfreetrial';
+    const authHeader = 'Basic ' + btoa(username + ':' + password);
     if (auth.getToken()) {
+
       const cloned =req.clone({
-        headers: req.headers.set('Authorization', `Bearer ${auth.getToken()}`)
+        headers: req.headers.set('Authorization', authHeader)
       });
       return next(cloned);
     }
